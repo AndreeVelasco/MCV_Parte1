313254abf8b28f460af9c2a3d1c4e11e7ba076e5
Andree Velasco <apandcan@gmail.com>
2020-08-30 18:50:06 -0500

    Culminacion 1ra Practica

--- MCV_Parte1/Controllers/PersonaController.cs
+++ MCV_Parte1/Controllers/PersonaController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MCV_Parte1.Models;
 
 namespace MCV_Parte1.Controllers
 {
     public class PersonaController : Controller
     {
         // GET: Persona
-        public ActionResult Index()
+        public ActionResult Persona()
         {
-            return View();
-        }
-        
+            ClsPersona obj = new ClsPersona();
+            obj.edad = 26;
+            obj.nombre = "Andree";
+            obj.apellido = "Velasco";
+            return View(obj);
+        }  
     }
 }
---
+++ MCV_Parte1/Models/ClsPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MCV_Parte1.Models
+{
+    public class ClsPersona
+    {
+        public int edad { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
